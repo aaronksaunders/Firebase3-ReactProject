edf0007ec1e9083aa268d037853ea17c132a969c
aaron k saunders <aaron@clearlyinnovative.com>
2017-02-28 21:25:30 -0500

    testing android support... it works

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Sample React Native App for File upload
  * https://github.com/facebook/react-native
  * @flow
  */
@@ -9,23 +9,89 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  Alert
 } from 'react-native';
 
+import ImagePicker from 'react-native-image-picker'
+
+import FirebaseService from './service/firebaseService'
+
 export default class RNFirebaseUploadApp extends Component {
+
+  _doShowAlert(_title = "RNFirebaseUploadApp", _message) {
+
+    // Works on both iOS and Android
+    Alert.alert(_title, _message,
+      [
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false }
+    )
+
+  }
+
+  _doPressAction() {
+    const options = {
+      title: 'Select Image',
+      noData: true,
+      quality: .8,
+      maxWidth: 2000,
+      maxHeight: 2000,
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    }
+
+    try {
+      ImagePicker.showImagePicker(options, (response) => {
+        //console.log('Response = ', response);
+
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else {
+          this.setState({ image: { uri: response.uri }, data: response })
+
+          FirebaseService.uploadFile(response).then((_resp) => {
+            console.log("file uploaded successfully", _resp);
+            this._doShowAlert("RNFirebaseUploadApp - Upload Success", _resp.downloadURL)
+          }, (_error) => {
+            console.log("Error Uploading File", _error);
+            this._doShowAlert("RNFirebaseUploadApp - Upload Error", JSON.stringify(_error, null, 2))
+          }).catch((_error) => {
+            console.log("Error Uploading File", _error);
+          })
+        }
+      });
+    } catch (EE) {
+      console.log(EE)
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native - Firebase File Upload Example
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
+        <Text style={[styles.welcome, { fontSize: 16 }]}>
+          Uploading file using RNFetchBlob
         </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor='#99d9f4'
+            onPress={() => { this._doPressAction() }}>
+            <Text style={styles.buttonText}>
+              GET PHOTO
+            </Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -43,11 +109,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  buttonText: {
+    fontSize: 12,
+    color: 'white',
+    alignSelf: 'center'
   },
+  button: {
+    marginTop: 10,
+    height: 32,
+    width: 120,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 2,
+    marginBottom: 10,
+    justifyContent: 'center'
+  }
 });
 
 AppRegistry.registerComponent('RNFirebaseUploadApp', () => RNFirebaseUploadApp);
